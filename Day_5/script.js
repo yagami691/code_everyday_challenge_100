b289f2dcc2beb489c60ad74d79803ad456076b41
yagami691 <pensnko@gmail.com>
2024-06-14 07:40:09 +0100

    blurring loading page

--- Day_5/script.js
+++ Day_5/script.js
@@ -1,15 +1,21 @@
-const loadingContainer = document.getElementById("loading-container");
-const loadingPercent = document.getElementById("loading-percent");
+const loadingContainer = document.querySelector(".loader-container");
+const loadingPercent = document.querySelector(".loading-percent");
 
 let load = 0;
+let loading = setInterval(blurred, 30);
 
-const loading = setInterval(blurred, 30);
-
-
-const blurred = ()=>{
- load++;
- if(load>99){
-    clearInterval(loading);
-    console.log(load)
- }
+function blurred(){
+    load++;
+    if(load>99){
+       clearInterval(loading);
+    }
+   loadingPercent.innerText = `${load}%`
+   loadingPercent.style.opacity = scale(load, 0, 100, 1, 0)
+   loadingContainer.style.filter = `blur(${scale(load, 0, 100, 30,0)}px)`
 }
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+
