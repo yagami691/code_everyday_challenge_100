e6c1126b5222e473e91df62cea0a577c9199be90
yagami691 <pensnko@gmail.com>
2024-06-16 00:32:39 +0100

    first release

---
+++ Day_7/script.js
@@ -0,0 +1,8 @@
+const leftPanel = document.querySelector(".left-panel");
+const rightPanel = document.querySelector(".right-panel");
+
+leftPanel.addEventListener("click",()=>{
+    leftPanel.classList.add("change-width");
+})
+
+
