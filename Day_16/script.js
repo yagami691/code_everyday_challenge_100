f1ff6eff732c06ed4e24e7810b9fc85022c84e87
yagami691 <pensnko@gmail.com>
2024-06-25 08:18:40 +0100

    first release

---
+++ Day_16/script.js
@@ -0,0 +1,125 @@
+const bottle1 = document.getElementById("bottle1");
+const bottle2 = document.getElementById("bottle2");
+const bottle3 = document.getElementById("bottle3");
+const bottle4 = document.getElementById("bottle4");
+const bottle5 = document.getElementById("bottle5");
+const bottle6 = document.getElementById("bottle6");
+const bottle7 = document.getElementById("bottle7");
+const bottle8 = document.getElementById("bottle8");
+const bottle9 = document.getElementById("bottle9");
+ 
+const moveLiter = document.querySelector(".move-liter")
+const twoLiters = document.querySelector(".two-liters")
+
+bottle1.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+
+bottle2.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+
+
+
+bottle3.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    bottle3.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+
+
+
+bottle4.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    bottle3.style.backgroundColor ="#5541d6";
+    bottle4.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+
+
+bottle5.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    bottle3.style.backgroundColor ="#5541d6";
+    bottle4.style.backgroundColor ="#5541d6";
+    bottle5.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+
+bottle6.addEventListener("click", ()=>{
+  
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    bottle3.style.backgroundColor ="#5541d6";
+    bottle4.style.backgroundColor ="#5541d6";
+    bottle5.style.backgroundColor ="#5541d6";
+    bottle6.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+bottle7.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    bottle3.style.backgroundColor ="#5541d6";
+    bottle4.style.backgroundColor ="#5541d6";
+    bottle5.style.backgroundColor ="#5541d6";
+    bottle6.style.backgroundColor ="#5541d6";
+    bottle7.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.innerHTML = `${(intLiter -(0.25))}`
+   
+
+
+})
+
+
+bottle8.addEventListener("click", ()=>{
+    bottle1.style.backgroundColor ="#5541d6";
+    bottle2.style.backgroundColor ="#5541d6";
+    bottle3.style.backgroundColor ="#5541d6";
+    bottle4.style.backgroundColor ="#5541d6";
+    bottle5.style.backgroundColor ="#5541d6";
+    bottle6.style.backgroundColor ="#5541d6";
+    bottle7.style.backgroundColor ="#5541d6";
+    bottle8.style.backgroundColor ="#5541d6";
+    intLiter = parseInt(moveLiter.innerHTML)
+    moveLiter.style.display ="none";
+   
+
+
+})
+
